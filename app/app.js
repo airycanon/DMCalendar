0c7b1d2315bb8cd520bdb0015c1cdeac043cb976
airycanon <airycanon@airycanon.me>
2015-02-27 16:17:47 +0800

    Change the First Day of the Week.

--- app/app.js
+++ app/app.js
@@ -2,11 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('dmCalendar', [
-  'ngRoute',
-  'dmCalendar.login',
-  'dmCalendar.register',
-  'dmCalendar.month',
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/month'});
+    'ngRoute',
+    'dmCalendar.login',
+    'dmCalendar.register',
+    'dmCalendar.month'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/month'});
 }]);
--- app/components/month.js
+++ app/components/month.js
@@ -1,39 +1,59 @@
 'use strict';
 
-angular.module('dmCalendar.month', ['ngRoute'])
+angular.module('dmCalendar.month', ['ngRoute','ui.bootstrap'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/month', {
             templateUrl: 'components/month.html',
             controller: 'monthCtrl'
         });
     }])
 
-    .controller('monthCtrl', ['$scope',function(scope) {
-        scope.month = getMonth(new Date());
+    .service('monthService', function () {
+        var monthService = {};
+        monthService.month = [[], [], [], [], [], [], []];
+        monthService.weekLabels = [];
 
-        function getMonth(date){
-            var month = [];
-            date.setDate(1);
-            console.log(date);
-            for(var i = 0;i < 6;i ++)
-            {
-                var start = new Date(date.getTime());
-                start.setDate(start.getDate() + 7 * i);
-                month[i] = getWeek(start);
+        monthService.refresh = function (date, firstDayOfWeek) {
+            var weekLabels = [['日', '一', '二', '三', '四', '五', '六'], ['一', '二', '三', '四', '五', '六', '日']];
+            var start = new Date(date.getTime());
+            start.setDate(1);
+            start.setTime(start.getTime() - 1000 * 24 * 60 * 60 * (7 - firstDayOfWeek));
+
+            for (var i = 0; i < 42; i++) {
+                var tempDate = new Date(start.getTime());
+                tempDate.setDate(tempDate.getDate() + i);
+                var week = parseInt(i / 7);
+                var day = i % 7;
+                if (week === 0) {
+
+                    monthService.weekLabels[tempDate.getDay()] = weekLabels[firstDayOfWeek][tempDate.getDay()];
+                }
+                monthService.month[week][day] = tempDate;
             }
-            return month;
+        };
+        return monthService;
+    })
+
+    .controller('monthCtrl', ['$scope', "monthService", function (scope, monthService) {
+        scope.firstDayOfWeek = 0;
+        scope.month = monthService.month;
+        scope.weekLabels = monthService.weekLabels;
+
+        var presentDay = new Date();
+        monthService.refresh(presentDay, scope.firstDayOfWeek);
+
+        scope.firstDayChange = function () {
+            monthService.refresh(presentDay, scope.firstDayOfWeek);
         }
 
-        function getWeek(date){
-            console.log("开始",date);
+        function getWeek(date, firstDayInWeek) {
             var week = [];
-            date.setDate(date.getDate() - date.getDay());
-            var start = new Date()
-            for(var i = 0;i < 7;i++){
+            date.setDate(date.getDate() - date.getDay() + firstDayInWeek);
+
+            for (var i = 0; i < 7; i++) {
                 var tempDate = new Date(date.getTime());
                 tempDate.setDate(tempDate.getDate() + i);
-                console.log("星期",i,tempDate);
                 week[i] = tempDate;
             }
             return week;
