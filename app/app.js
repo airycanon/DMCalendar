a011670ff3e84c7ef048aa516a5835fd6bea4acd
airycanon <airycanon@airycanon.cn>
2015-03-01 00:38:59 +0800

    Add week display.

--- app/app.js
+++ app/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('dmCalendar', [
     'ngRoute',
     'dmCalendar.login',
     'dmCalendar.register',
-    'dmCalendar.month'
-]).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/month'});
-}]);
+    'dmCalendar.controller',
+    'dmCalendar.service'
+]);
