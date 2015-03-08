051497070bfb5d8c507072da742b7c6f78674048
airycanon <airycanon@airycanon.me>
2015-03-09 01:05:01 +0800

    Current day highlight.

--- app/controller.js
+++ app/controller.js
@@ -20,13 +20,19 @@ angular.module('dmCalendar.controller', ['ui.bootstrap'])
             }
         }
 
-        scope.check = function (date){
-            if(date.getMonth() === presentDay.getMonth()){
-                return 'present-cell';
-            } else if(date.getMonth() < presentDay.getMonth()){
-                return 'previous-cell';
-            } else {
-                return 'next-cell';
+        scope.checkMonth = function (date){
+            var className = "";
+            if(date.getYear() == presentDay.getYear() && date.getMonth() == presentDay.getMonth() && date.getDate() == presentDay.getDate()){
+                className += "current-cell ";
             }
+            if(date.getMonth() === presentDay.getMonth()){
+                className += 'present-month-cell';
+            } else if(date.getMonth() < presentDay.getMonth()){
+                className +=  'previous-month-cell';
+            } else {
+                className += 'next-month-cell';
+            }
+
+            return className;
         }
     }]);
